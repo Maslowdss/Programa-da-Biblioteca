62771fbe29f738696561549d4d89fe135203b5a3
Maslowdss <maslowdss2002@gmail.com>
2024-09-26 20:24:22 -0300

    Melhoramento do codigo, conto com vocês para terminarem as coisas para a primeira apresentação

--- Biblioteca/main.c
+++ Biblioteca/main.c
@@ -1,4 +1,4 @@
-// Bibliotecas
+//Bibliotecas
 #include <stdio.h>      // Para entrada e saída padrão (printf, scanf, etc.)
 #include <stdlib.h>     // Para funções de utilidade geral (malloc, free, rand, srand, etc.)
 #include <string.h>     // Para manipulação de strings (strlen, strcpy, strcmp, etc.)
@@ -21,40 +21,53 @@
 // Variáveis Globais
 // As variáveis padrão/default são utilizadas para evitar a criação excessiva de variáveis ao longo do programa.
 // Elas são projetadas para usos temporários e podem perder seu valor logo após a utilização.
-bool defaultStatusFlag = false;          // Indica o estado atual de uma condição qualquer (pode ser login, autenticação, etc.)
-int defaultInteger = -1;                 // Inteiro padrão utilizado como valor inicial
-float defaultFloat = -1.1;               // Float padrão utilizado como valor inicial
-char defaultCharacter = 'a';             // Caractere padrão utilizado como valor inicial
-char defaultStringVector[1000];          // Buffer para armazenar uma string longa
-char defaultStringMatrix[1000][1000];    // Matriz de caracteres para armazenamento de dados bidimensionais
+bool defaultStatusFlag = true;     // Indica o estado atual de uma condição qualquer (pode ser login, autenticação, etc.)
+int defaultInteger = 0;            // Inteiro padrão utilizado como valor inicial
+float defaultFloat = 0;            // Float padrão utilizado como valor inicial
+char defaultCharacter = 'a';       // Caractere padrão utilizado como valor inicial
+char defaultStringVector[0];       // Vetor para armazenar uma string
+char defaultStringMatrix[0][0];    // Matriz de caracteres para armazenamento de dados bidimensionais
 
-char userLogin[25];                      // Armazena o login do usuário
-char userPassword[25];                   // Armazena a senha do usuário
-char userCategory[25];                   // Informa a categoría do usuário
+int counterInfiniteLoop = 0;       // Contador que limita o número de iterações de um loop
 
-char loginList[200][20];                 // Lista de logins de até 200 usuários, com 20 caracteres cada
-char passwordList[200][20];              // Lista de senhas correspondentes, com 20 caracteres cada
-char userCategoryList[200][20];          // Lista de categorias correspondentes a cada usuário
-int existingAccounts = 0;                // Contador para o número de usuários registrados
+char userLogin[12];                // Armazena o login do usuário
+char userPassword[12];             // Armazena a senha do usuário
+char userCategory[12];             // Informa a categoría do usuário
 
-char bookName[100];                      // Armazena o nome de um livro, com até 100 caracteres
-char bookAuthor[100];                    // Armazena o nome do autor do livro, com até 100 caracteres
-char bookCategory[20];                   // Armazena a categoria do livro, com até 20 caracteres
+char loginList[0][12];             // Lista de logins de até 0 usuários, com 11 caracteres cada
+char passwordList[0][12];          // Lista de senhas correspondentes, com 11 caracteres cada
+char userCategoryList[0][12];      // Lista de categorias correspondentes a cada usuário
+int existingAccounts = 0;          // Contador para o número de usuários registrados
 
-char bookNameList[200][100];             // Lista de nomes de até 200 livros, cada um com até 100 caracteres
-char bookAuthorList[200][100];           // Lista de autores correspondentes aos livros, com até 100 caracteres cada
-char bookCategoryList[200][20];          // Lista de categorias correspondentes aos livros, com até 20 caracteres cada
+char bookName[22];                 // Armazena o nome de um livro, com até 21 caracteres
+char bookAuthor[22];               // Armazena o nome do autor do livro, com até 21 caracteres
+char bookCategory[22];             // Armazena a categoria do livro, com até 21 caracteres
+
+char bookNameList[0][22];          // Lista de nomes de até 0 livros, cada um com até 21 caracteres
+char bookAuthorList[0][22];        // Lista de autores correspondentes aos livros, com até 21 caracteres cada
+char bookCategoryList[0][22];      // Lista de categorias correspondentes aos livros, com até 21 caracteres cada
+int existinBooks = 0;              // Contador para a quantidade de livros cadastrados
 
 
 // Limpa o buffer de entrada, descartando todos os caracteres até a nova linha ou EOF
 void ClearBuffer()
 {
-    defaultInteger = 1;    // Variável para armazenar os caracteres lidos do buffer
+    int defaultInteger = 1;         // Variável para armazenar os caracteres lidos do buffer
+    int counterInfiniteLoop = 0;    // Conta até chegar em 100000, se isso acontecer ele cancela o loop while
 
-    // Lê caracteres do buffer até encontrar uma nova linha ou o final do arquivo
     while(defaultInteger != '\n' && defaultInteger != EOF)
     {
-        defaultInteger = getchar();
+        defaultInteger = getchar();    // Lê caracteres do buffer até encontrar uma nova linha ou o final do arquivo
+
+        counterInfiniteLoop++;
+
+        //Se o loop while atingir 1000 interações, ele será interrompido, permitindo que o programa continue sem erros.
+        if(1000 <= counterInfiniteLoop)
+        {
+            counterInfiniteLoop=0;
+
+            return;
+        }
     }
 
     return;
@@ -62,46 +75,61 @@ void ClearBuffer()
 
 
 // Imprime cada caractere da string com um tempo de atraso especificado em Milissegundos.
-// Permite personalização da string de finalização. Use "\n" para pular linha e "" para fazer nada.
+// Permite personalização da string de finalização. Use "\n" para pular linha e "" ou '\0' para fazer nada.
 void PrintWithDelay(const char defaultStringVector[], int delay, char endChar[])
 {
-    // Verifica se o vetor de caracteres não está vazio
-    if (defaultStringVector == NULL || delay < 0)
+    if (defaultStringVector == NULL)    // Verifica se o vetor de caracteres não está vazio
     {
-        printf("Erro: string inválida ou atraso negativo.\n");
+        printf("Erro: O vetor de caracteres fornecido para a função 'PrintWithDelay' é inválido.\n");
         return;    // Retorna se a entrada for inválida
+    }
+    else if (delay < 0)    // Verifica se o delay é negativo
+    {
+        printf("Erro: O valor de delay fornecido para a função 'PrintWithDelay' não pode ser negativo.\n");
+        return;
     }
 
     int length = strlen(defaultStringVector);    // Obtém o comprimento da string
 
-    for (int i = 0; i < length; i++) {
-        printf("%c", defaultStringVector[i]);   // Imprime o caractere atual
-        fflush(stdout);                         // Garante que o caractere seja exibido imediatamente
-        Sleep(delay);                           // Pausa a execução pelo tempo especificado
+    for (int i = 0; i < length; i++)
+    {
+        printf("%c", defaultStringVector[i]);    // Imprime o caractere atual
+        fflush(stdout);                          // Garante que o caractere seja exibido imediatamente
+        Sleep(delay);                            // Pausa a execução pelo tempo especificado
     }
 
-    printf("%s", endChar);    // Imprime o string de finalização
-    fflush(stdout);
+    if (endChar != NULL)
+    {
+        printf("%s", endChar);    // Imprime a string de finalização
+        fflush(stdout);
+    }
 
     return;
 }
 
 
-// Muda a cor do texto no console
-// Parâmetro: color - valor WORD representando a cor desejada
-// Escreva um número de 0 a 15 para a trocar a cor de texto. 7 é branco
+// Muda a cor do texto no console.
+// Parâmetro: color - valor WORD representando a cor desejada (0 a 15).
+// Utilize um número entre 0 e 15 para trocar a cor do texto. O valor 7 é branco.
 void SetColor(WORD color)
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);  // Obtém o identificador do console padrão.
+    // Verifica se o valor de cor está dentro do intervalo permitido
+    if (color < 0 || color > 15)
+    {
+        printf("Erro: O valor da cor deve estar entre 0 e 15.\n");
+        return;
+    }
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);    // Obtém o identificador do console padrão.
 
     // Verifica se o handle é válido.
     if (hConsole == INVALID_HANDLE_VALUE)
     {
-        printf("Erro ao obter o identificador do console.\n");
+        printf("Erro: falha ao obter o identificador do console.\n");
         return;
     }
 
-    SetConsoleTextAttribute(hConsole, color); // Define a cor do texto no console.
+    SetConsoleTextAttribute(hConsole, color);    // Define a cor do texto no console.
 
     return;
 }
@@ -110,23 +138,10 @@ void SetColor(WORD color)
 // Principal
 int main()
 {
-    srand(time(NULL));    // Define a semente do gerador de números aleatórios com base no tempo atual
-    setlocale(LC_CTYPE, "Portuguese_Brazil");    // Permitinde o uso de caracteres acentuados
+    // Configurações Iniciais
+    srand(time(NULL));                           // Inicializa a semente para geração de números aleatórios
+    setlocale(LC_CTYPE, "Portuguese_Brazil");    // Configura a acentuação e caracteres especiais em português
+    SetColor(15);                                // Define a cor do texto para branco brilhante (gostei dela)
 
-    // Exemplo do uso das funções, retirar depois.
-    PrintWithDelay("[Projeto da Biblioteca]", 100, "\n\n"); //Exemplo do uso da função, a linha pode ser retirada
-    printf("= Criadores =\n\n");
-    SetColor(6);
-    PrintWithDelay("1 - Arthur", 80, "\n");
-    SetColor(10);
-    PrintWithDelay("2 - Rebeca", 80, "\n");
-    SetColor(9);
-    PrintWithDelay("3 - Maslow", 80, "");
-    SetColor(7);
-
-    Sleep(3000);
-    printf("\n\nGalera, lembrar de modificar essa introdução depois, isso é so um exemplo das funções que eu criei.\n\n");
-    // Apaga daqui até o inicio do exemplo
-
-    return 0;
+    return 0;    // Finaliza o programa
 }
